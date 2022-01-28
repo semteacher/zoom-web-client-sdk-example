744ee9f68c49a674b19878c492a67c4051466da0
Andriy Semenets <andriysemenets@meridianuniversity.edu>
2022-01-28 19:08:39 +0200

    move keys into directory up from www

---
+++ keys.php
@@ -0,0 +1,10 @@
+<?php
+
+// fill in your API Secret (probably from the JWT, not the SDK!)
+$apiKey         = 'DSj0kSohSHKkqjkbVpfEtw'; 
+$apiSecret 		= 'zmFxo5ShNIhFxHh5zcVEREj9Mtc4AeBmb1Iw';
+$meetingNumber  = '96881738675';
+$meetingPwd     = '1d7zNq';
+$leaveUrl       = 'index.html';
+
+?>
